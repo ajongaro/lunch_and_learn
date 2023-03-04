24b8830ee5ec8308f7cd463b2ae8cd21ef1b461a
Anthony Ongaro <43623494+ajongaro@users.noreply.github.com>
2023-03-04 10:05:15 -0700

    test: Add simplecov and .gitignore for /coverage

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,9 +1,18 @@
 class RecipesController < ApplicationController
   def index
-    binding.pry
-    # Get Country Param
-    # If no country param, call REST Countries API to get random country
-    # Call on the recipe API service to get all recipes from random country or query param
-    # Serialize the response
+    if recipe_params
+      # Call on the recipe API service to get all recipes from query param
+      # Serialize the response
+    else
+      # If no country param, call REST Countries API to get random country
+      # Call on the recipe API service to get all recipes from random country
+      # Serialize the response
+    end
+  end
+
+  private
+
+  def recipe_params
+    params.permit(:country)
   end
 end
