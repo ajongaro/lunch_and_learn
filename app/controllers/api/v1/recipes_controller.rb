934ce2e2354c89389be5dc3db9ba2dc5c185bcab
Anthony Ongaro <43623494+ajongaro@users.noreply.github.com>
2023-03-05 21:03:02 -0700

    fix: clean up json response with passing tests

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,22 +1,35 @@
 class Api::V1::RecipesController < ApplicationController
   def index
-    if recipe_params[:country] == ''
-      {
-        "data": []
-      }
-    elsif recipe_params[:country]
-      recipes = RecipeFacade.recipes_for(recipe_params[:country])
-      render json: RecipeSerializer.new(recipes).serializable_hash
-    else
-      country = CountryService.random_country
-      recipes = RecipeFacade.recipes_for(country)
-      render json: RecipeSerializer.new(recipes).serializable_hash
-    end
+    return recipes_from_random_country unless params[:country]
+    return no_data if no_country?
+
+    return_recipes(searched_recipes_response)
   end
 
   private
 
-  def recipe_params
-    params.permit(:country)
+  def no_country?
+    params[:country].strip == ''
+  end
+
+  def no_data
+    render json: { "data": [] }
+  end
+
+  def return_recipes(recipes)
+    return no_data if recipes.nil?
+
+    recipes
+  end
+
+  def searched_recipes_response
+    recipes = RecipeFacade.recipes_for(params[:country])
+    render json: RecipeSerializer.new(recipes)
+  end
+
+  def recipes_from_random_country
+    country = CountryService.random_country
+    recipes = RecipeFacade.recipes_for(country)
+    render json: RecipeSerializer.new(recipes)
   end
 end
